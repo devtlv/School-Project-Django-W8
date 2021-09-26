6599ef686a04d380ff47eeabdcb25481c279c35b
LiseNakache <nakache.lise@gmail.com>
2021-09-26 17:31:00 +0300

    PartI

--- README.md
+++
@@ -1,4 +0,0 @@
-# School-Project-Django-W8
-
-
-A project using Django, done during the last two weeks of the Bootcamp
--- school_project_Part5/exams/static/exams/js/script.js
+++
@@ -1,5 +0,0 @@
-let button = document.getElementById('mybutton')
-
-button.addEventListener('click', function(){
-    alert("You clicked me!")
-})
